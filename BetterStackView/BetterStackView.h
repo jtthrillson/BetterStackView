5aa30aeef7d9e3007851d0f8dc9b39be7ddf8cc0
John Wilson <jtthrillson@gmail.com>
2020-06-12 14:06:03 +0100

    Initial Commit

---
+++ BetterStackView/BetterStackView.h
@@ -0,0 +1,19 @@
+//
+//  BetterStackView.h
+//  BetterStackView
+//
+//  Created by John Wilson on 12/06/2020.
+//  Copyright © 2020 DangerBarrel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BetterStackView.
+FOUNDATION_EXPORT double BetterStackViewVersionNumber;
+
+//! Project version string for BetterStackView.
+FOUNDATION_EXPORT const unsigned char BetterStackViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BetterStackView/PublicHeader.h>
+
+
